5a0366d48ab717354ec857e1fc67884286f9f5c8
AyushmaanAggarwal <ayu02468@gmail.com>
2023-04-30 16:10:09 -0700

    Initial commit

---
+++ sendEmail.py
@@ -0,0 +1,34 @@
+import smtplib
+import ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def gmail_send_message(subject, message_text):
+
+    with open("../instance/pythonpass", 'r') as file:
+        email_password = file.read()
+
+    with open("../instance/toEmail", 'r') as file:
+        to_address = file.read()
+        
+    with open("../instance/fromEmail", 'r') as file:
+        from_address = file.read()
+        
+    em = MIMEMultipart('alternative')
+    em['From'] = from_address
+    em['To'] = to_address
+    em['Subject'] = subject
+    part2 = MIMEText(message_text, 'html')
+    em.attach(part2)
+
+    context = ssl.create_default_context()
+
+    try:
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(from_address, email_password)
+            smtp.sendmail(from_address, to_address, em.as_string())
+        return "Email Sent!"
+    except:
+        return "Email failed to send"
+    
